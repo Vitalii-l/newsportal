297221e8304826d4edfb927e2c3a5dcaf18b2cc6
Vitalii-l <5754711@gmail.com>
2020-03-30 17:46:41 +0300

    Админ панель: починил вход и отображение админки - не хватало .htaccess

--- admin/viewAdmin/formLogin.php
+++ admin/viewAdmin/formLogin.php
@@ -8,9 +8,10 @@ if (isset($_SESSION['userId'])) {
     <head>
         <title>Admin dashboard</title>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="../../css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-        <link href="../..css/login.css" rel="stylesheet" type="text/css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">        
+        <link href="./css/mystyle.css" rel="stylesheet" type="text/css" />
+        <link href="./css/login.css" rel="stylesheet" type="text/css" />
+        <link href="./css/bootstrap.min.css" rel="stylesheet" type="text/css" />
     </head>
     <body>
         <div class="container">
--- admin/viewAdmin/templates/layout.php
+++ admin/viewAdmin/templates/layout.php
@@ -3,11 +3,11 @@
     <head>
         <title>Dashboard</title>
         <meta charset="utf-8">
-        <link href="../../../css/style.css" rel="stylesheet" type="text/css"/>
-        <link href="../../../css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-        <link href="../../../css/all.css" rel="stylesheet" type="text/css"/>
-        <script src="../../../js/jquery-3.4.1.min.js" type="text/javascript"></script>
-        <script src="../../../js/bootstrap.min.js" type="text/javascript"></script>
+        <link href="./css/mystyle.css" rel="stylesheet" type="text/css" />
+        <link href="./css/login.css" rel="stylesheet" type="text/css" />
+        <link href="./css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <script src="./js/jquery-3.4.1.min.js" type="text/javascript"></script>
+        <script src="./js/bootstrap.min.js" type="text/javascript"></script>
 <!--        <script src="js/popper.min.js"></script>-->
     </head>
     <body>
