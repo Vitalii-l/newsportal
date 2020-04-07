c6dbb65eee250f7e0c53d15c409dfd972d77a584
Vitalii-l <5754711@gmail.com>
2020-04-07 10:42:28 +0300

    В процессе добавления функционала "Добавить новость". Не работает добавление, не выводятся категории в форме добавления новости. (задание Uudised-News_MVC6-NewsAddAdmin.pdf)

--- admin/viewAdmin/newsAddForm.php
+++ admin/viewAdmin/newsAddForm.php
@@ -1,8 +1,74 @@
-<?php
+<?php ob_start(); ?>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<div class="container" style="min-height: 400px;">
+    <div class="col-md-11">
+        <h2>News Add</h2>
+        <?php
+        if(isset($test)){
+            if($test == true){
+        ?>
+                <div class="alert alert-info">
+                    <strong>News added.</strong><a href="newsAdmin">News List</a>
+                </div>
+        <?php
+            }
+        else if ($test == false) {
+            ?>
+            <div class="alert alert-warning">
+                <strong>An error occurred while trying to add news</strong><a href="newsAdmin">News List</a>
+            </div>
+        <?php
+        }
+        }
+        else {            
+        ?>
+        
+        <form method="POST" action="newsAddResult" enctype="multipart/form-data">
+            <table class="table table-bordered">
+                <tr>
+                    <td>News title</td>
+                    <td><input type="text" name="title" class="form-control" required></td>
+                </tr>
+                <tr>
+                    <td>News text</td>
+                    <td><textarea rows="5" name="text" class="form-control" required></textarea></td>
+                </tr>
+                <tr>
+                    <td>Category</td>
+                    <td>
+                        <select name="idCategory" class="form-control">
+                            <?php
+                                    foreach ($arr as $$row) {
+                                        echo '<option value="'.$row['id'].'">'.$row['name'].'</option>';
+                                    }
+                            ?>            
+                        </select>
+                    </td>
+                </tr>
+                <!-- image -->
+                <tr>
+                    <td>Picture</td>
+                    <td><div>
+                            <input type="file" name="picture" style="color:black;">
+                        </div>
+                    </td>
+                </tr>
+                <!-- end image -->
+                <tr>
+                    <td colspan="2">
+                        <button type="submit" class="btn btn-primary" name="save">
+                            <span class="glyphicon glyphicon-plus"></span>Save
+                        </button>
+                        <a href="newsAdmin" class="btn btn-large btn-success">
+                            <i class="glyphicon glyphicon-backward"></i></a>
+                    </td>
+                </tr>
+            </table>
+        </form>
+        <?php } ?>
+        
+    </div>
+</div>
+<?php $content = ob_get_clean();
+include "viewAdmin/templates/layout.php";?>
 
